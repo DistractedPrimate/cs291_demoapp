dfb247535c2e98d39cd5a1724c2b6659b3eb8a14
DistractedPrimate <m_medearis@ucsb.edu>
2022-10-21 13:05:02 -0700

    added 404 page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  match "/404", to: "errors#not_found", via: :all
   # Defines the root path route ("/")
   # root "articles#index"
 end
